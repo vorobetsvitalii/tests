36038af41a5595b4dcec3e4211e328e66c599e6a
vorobetsvitalii <vorobetsvitaliy3@gmail.com>
2023-09-18 13:50:48 +0300

    Add Unit tests

---
+++ lab1/Calculator/UnitTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed19a3cc-fe0c-4ae7-b0a4-0187792cd74a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ lab1/Calculator/UnitTest1/UnitTest1.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Linq;
+using System.Data.SqlClient;
+using AnalaizerClassLibrary;
+
+namespace YourNamespace.Tests
+{
+    [TestClass]
+    public class AnalyzerTests
+    {
+
+        private const string ConnectionString = "Server=HOME-PC;Integrated Security=True;Database=Tests";
+
+        [TestMethod]
+        public void TestCreateStackWithOperatorsOnly()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT InputExpression, PolishExpression FROM TestCreateStackWithOperatorsOnly";
+                using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string expression = reader.GetString(0);
+                        string expected = reader.GetString(1);   
+
+                        // Act
+                        ArrayList result = AnalaizerClass.CreateStack(expression);
+
+                        // Assert
+                        ArrayList expectedList = new ArrayList(expected.Split(' '));
+                        CollectionAssert.AreEqual(expectedList.ToArray(), result.ToArray(), GetAssertionMessage(expectedList, result));
+                    }
+                }
+            }
+        }
+
+
+        [TestMethod]
+        public void TestCreateStackWithBrackets()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT InputExpression, PolishExpression FROM TestCreateStackWithBrackets";
+                using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string expression = reader.GetString(0); 
+                        string expected = reader.GetString(1);  
+
+                        // Act
+                        ArrayList result = AnalaizerClass.CreateStack(expression);
+
+                        // Assert
+                        ArrayList expectedList = new ArrayList(expected.Split(' '));
+                        CollectionAssert.AreEqual(expectedList.ToArray(), result.ToArray(), GetAssertionMessage(expectedList, result));
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestCreateStackWithDifferentOperators()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT InputExpression, PolishExpression FROM TestCreateStackWithDifferentOperators";
+                using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string expression = reader.GetString(0); 
+                        string expected = reader.GetString(1); 
+
+                        // Act
+                        ArrayList result = AnalaizerClass.CreateStack(expression);
+
+                        // Assert
+                        ArrayList expectedList = new ArrayList(expected.Split(' '));
+                        CollectionAssert.AreEqual(expectedList.ToArray(), result.ToArray(), GetAssertionMessage(expectedList, result));
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestCreateStackWithEmptyExpression()
+        {
+            // Arrange
+            string expression = "";
+
+            // Act
+            ArrayList result = AnalaizerClass.CreateStack(expression);
+
+            // Assert
+            ArrayList expected = new ArrayList();
+            CollectionAssert.AreEqual(expected.ToArray(), result.ToArray(), GetAssertionMessage(expected, result));
+        }
+
+        [TestMethod]
+        public void TestCreateStackWithComplexExpression()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT InputExpression, PolishExpression FROM TestCreateStackWithComplexExpression";
+                using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string expression = reader.GetString(0); 
+                        string expected = reader.GetString(1);  
+
+                        // Act
+                        ArrayList result = AnalaizerClass.CreateStack(expression);
+
+                        // Assert
+                        ArrayList expectedList = new ArrayList(expected.Split(' '));
+                        CollectionAssert.AreEqual(expectedList.ToArray(), result.ToArray(), GetAssertionMessage(expectedList, result));
+                    }
+                }
+            }
+        }
+
+        private string GetAssertionMessage(ArrayList expected, ArrayList actual)
+        {
+            return $"Expected: [{string.Join(", ", expected.Cast<string>())}], Actual: [{string.Join(", ", actual.Cast<string>())}]";
+        }
+    }
+}
